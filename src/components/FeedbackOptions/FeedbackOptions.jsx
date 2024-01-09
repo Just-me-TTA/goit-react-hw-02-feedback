4c171756d6ad4d8b2d834d5db74108ce5171bdba
Just-me-TTA <129186821+Just-me-TTA@users.noreply.github.com>
2024-01-09 22:15:02 +0200

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,26 +1,18 @@
-import React, { useState } from 'react';
-import { nanoid } from 'nanoid';
+import React from 'react';
 import { Button, ButtonList } from './FeedbackOptions.styled';
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-  const [keys, setKeys] = useState(options.map(() => nanoid()));
-
-  const regenerateKeys = () => {
-    setKeys(options.map(() => nanoid()));
-  };
+  const uniqueKey = 'feedback-option'; 
 
   return (
     <ButtonList>
-      {options.map((option, index) => (
-        <li key={keys[index]}>
+      {options.map(option => (
+        <li key={`${uniqueKey}-${option}`}>
           <Button type="button" aria-label={option} onClick={() => onLeaveFeedback(option)}>
             {option}
           </Button>
         </li>
       ))}
-      <button type="button" onClick={regenerateKeys} style={{ marginTop: '10px' }}>
-        Regenerate Keys
-      </button>
     </ButtonList>
   );
 };
